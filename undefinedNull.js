fa7f382c42ba8016fcc1cf9935f5fd6cbf9f2c7f
hasanfuad <hasanmdfuad@hotmail.com>
2021-07-01 21:23:25 +0600

    undefined and null commit

---
+++ undefinedNull.js
@@ -0,0 +1,16 @@
+
+let value = [29,21,2122,43]; 
+
+function add(num1,num2){
+    // console.log(num1+num2);
+}
+
+const obj = {
+    id: 23,
+    oName: "fuad"
+}
+console.log(obj.age);
+
+const age = null; //We explicitly set Null to ensure nothing left on the list
+
+console.log(age);
